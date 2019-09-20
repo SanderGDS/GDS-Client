208a8825fee6430d7268b0c114709a281fcfb1c4
SanderGDS <55578712+SanderGDS@users.noreply.github.com>
2019-09-20 10:23:16 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,22 +1,54 @@
 xui.Class('App', 'xui.Module',{
     Instance:{
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Tools
+            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
-            append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout5")
-            .setItems([{"id":"before", "pos":"before", "size":100, "min":50, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}, {"id":"main", "min":10, "size":450}, {"id":"after", "pos":"after", "size":50, "min":20, "max":400, "locked":false, "folded":false, "hidden":false, "cmd":false}])
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel4")
+                .setDock("none")
+                .setLeft("0em")
+                .setTop("0em")
+                .setWidth("66.66666666666667em")
+                .setHeight("50em")
+                .setCaption("Grand Dog Studio Client")
             );
-
-            host.ctl_layout5.append((new xui.UI.Layout())
-            .setHost(host,"ctl_layout13")
-            .setItems([{"id":"before", "pos":"before", "size":150, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":true}, {"id":"main", "min":10, "size":530}, {"id":"after", "pos":"after", "size":120, "min":10, "locked":false, "folded":false, "hidden":false, "cmd":false}])
-            .setType("horizontal")
-            , "main");
-
+            
+            host.xui_ui_panel4.append(
+                xui.create("xui.UI.ButtonViews")
+                .setHost(host,"xui_ui_buttonviews2")
+                .setItems([
+                    {
+                        "id":"a",
+                        "caption":"Home",
+                        "imageClass":"xui-icon-xui"
+                    },
+                    {
+                        "id":"b",
+                        "caption":"Libary"
+                    },
+                    {
+                        "id":"c",
+                        "caption":"Support"
+                    },
+                    {
+                        "id":"d",
+                        "caption":"Forums",
+                        "closeBtn":true,
+                        "optBtn":"xui-uicmd-opt",
+                        "popBtn":true
+                    }
+                ])
+                .setLeft("0em")
+                .setTop("0em")
+                .setBarLocation("left")
+                .setBarSize("11.666666666666666em")
+                .setValue("a")
+            );
+            
             return children;
-            // ]]Code created by CrossUI RAD Tools
+            // ]]Code created by CrossUI RAD Studio
         }
     }
 });
